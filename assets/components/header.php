13313353f978e8120b6a0e363a0a6ca02764257f
workOfYasir <yasirb673@gmail.com>
2021-06-01 17:05:50 +0500

    basic design
    Signed-off-by: workOfYasir <yasirb673@gmail.com>

---
+++ assets/components/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Optional theme -->
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"> -->
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+<!-- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="" href="./assets/css/style.css">
+    <title>Document</title>
+   
+</head>
+<body> 
+<div class="container-fluid">
+
+    <div class="row">
+<div class="col-md-12  col-sm-12 col-xs-12 nav">
+<div class="row" style="margin-top: 2px; margin-bottom:2px">
+        <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+<span style="font-size:12px; font-weight:700; color:black;">ADVERTORIAL</span>
+        </div>
+    </div>
+    <div class="header">
+        <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12" >
+            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12" >
+<div class='outside-wrapper '>
+<a data-reload="1" href="javascript:void(0)">
+<span class='hidden-xs hidden-sm hidden-md image '>
+<img alt="Logo" width="223" height="20" style="height: auto;" srcset="./assets/img/headerLogo1.png 1x, ./assets/img/headerLogo1.png 2x" src="./assets/img/headerLogo1.png" />
+</span>
+<span class='image visible-xs visible-sm visible-md text-center'>
+<img alt="Logo" width="200" height="15" style="height: auto;" srcset="./assets/img/headerLogo2.png 1x, ./assets/img/headerLogo2.png 2x" src="/assets/img/headerLogo2.png" />
+</div>
+</span>
+</a>
+</div>
+<div class="col-lg-4 col-md-6  col-md-offset-4 col-lg-offset-2 text-div hidden-sm hidden-xs hidden-md visible-lg-block  " style="margin-top:2%">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-right text ">
+<strong class="strong-nav" style="color: #fff; font-weight:500 !important">
+Money Saving Trends & More</strong></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
